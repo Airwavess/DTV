fc61f434c11f9db37162eaa09061f7d67f5bc2c0
Leo <prairie5270@gmail.com>
2017-05-20 17:20:44 +0800

    Add RWD

--- static/js/scrolling parallax.js
+++ static/js/scrolling parallax.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
 	var ori_scroll_top = $(document).scrollTop()
   $(document).scroll(function() {
     $(".jumbotron").css("background-position", 'center ' + (-50 + ($(document).scrollTop() * 0.2) + 'px'));
-    $(".slide-2").css("background-position", 'center ' + (-300 + ($(document).scrollTop() * 0.2) + 'px'));
+    $(".slide-2").css("background-position", 'center ' + (-500 + ($(document).scrollTop() * 0.2) + 'px'));
+    console.log((-500 + ($(document).scrollTop() * 0.2)))
   });
   // $(".jumbotron").css("height", $(window).height())
 });
