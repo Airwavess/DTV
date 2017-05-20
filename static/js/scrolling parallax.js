37a520d4dc1a1032b3072e5e8a41ee97d4a19560
Leo <prairie5270@gmail.com>
2017-05-20 18:36:34 +0800

    Fix srolling BUG

--- static/js/scrolling parallax.js
+++ static/js/scrolling parallax.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
-	var ori_scroll_top = $(document).scrollTop()
+  var ori_scroll_top = $(document).scrollTop()
+  var bg_position_jumbotron = parseInt($('.jumbotron').css("background-position").split(" ")[1].split("px")[0])
+  var bg_position_slide = parseInt($('.slide-2').css("background-position").split(" ")[1].split("px")[0])
+  $(".jumbotron").css("background-position", 'center ' + (bg_position_jumbotron + ($(document).scrollTop() * 0.2) + 'px'));
+  $(".slide-2").css("background-position", 'center ' + (bg_position_slide + ($(document).scrollTop() * 0.2) + 'px'));
+  // bg_position_jumbotron = parseInt($('.jumbotron').css("background-position").split(" ")[1].split("px")[0])
+  // bg_position_slide = parseInt($('.slide-2').css("background-position").split(" ")[1].split("px")[0])
   $(document).scroll(function() {
-    $(".jumbotron").css("background-position", 'center ' + (-50 + ($(document).scrollTop() * 0.2) + 'px'));
-    $(".slide-2").css("background-position", 'center ' + (-500 + ($(document).scrollTop() * 0.2) + 'px'));
-    console.log((-500 + ($(document).scrollTop() * 0.2)))
+    $(".jumbotron").css("background-position", 'center ' + (bg_position_jumbotron + ($(document).scrollTop() * 0.2) + 'px'));
+    $(".slide-2").css("background-position", 'center ' + (bg_position_slide + ($(document).scrollTop() * 0.2) + 'px'));
   });
-  // $(".jumbotron").css("height", $(window).height())
 });
