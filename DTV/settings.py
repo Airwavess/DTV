8d0855b69fb581b717aabbc989f1b7b81c772667
leo <prairie5270@gmail.com>
2017-05-22 23:38:56 +0800

    Fix the lost instruction - 'load staticfiles'

--- DTV/settings.py
+++ DTV/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '52q5^@0$6xqz!+&*#40xr!9#($v_=mqk^e=-j%swg!802yh&zg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
